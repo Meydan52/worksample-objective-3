c171a266623c474267033e3fd46b4d8a7ba10135
Meydan52 <nizamimeydan@gmail.com>
2022-10-25 21:42:30 -0400

    bucket name fixed

--- output.tf
+++ output.tf
@@ -4,18 +4,27 @@ output "s3_bucket" {
   description = "Bucket"
 }
 
+# Make the bucket name reachabel to upload files to S3. 
+output "bucket_name" {
+  value       = aws_s3_bucket.example.name
+  description = "Access key id for test user"
+}
+
+
 # Expose bucket region
 output "bucket_region" {
   value       = aws_s3_bucket.example.region
   description = "s3 bucket region"
   sensitive   = true
 }
+
 # Make the key can be reachabel to upload files to S3. 
 output "access_key_id" {
   value       = aws_iam_access_key.s3-user-key.id
   description = "Access key id for test user"
   sensitive   = true
 }
+
 
 # value       = base64encode(aws_iam_access_key.s3-user-key.secret)
 output "secret_access_key" {
